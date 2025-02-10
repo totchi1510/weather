c5e2e1f4acd9b974fe30f797be078b1eaec261cf
totchi1510 <179804262+totchi1510@users.noreply.github.com>
2025-02-11 00:50:16 +0900

    Update app.js

--- app.js
+++ app.js
@@ -13,3 +13,13 @@ async function fetchWeather() {
 }
 
 fetchWeather();
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js')
+    .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch(error => {
+        console.error('Service Worker registration failed:', error);
+    });
+}
